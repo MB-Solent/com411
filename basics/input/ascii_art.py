491265044241fb5653e79fabc43528eca41fe56a
Mihai Badea <4badem14@solent.ac.uk>
2022-09-29 12:44:05 +0100

    moved file to output folder, added user input for characters for the eyes

---
+++ basics/input/ascii_art.py
@@ -0,0 +1,12 @@
+# Display a box
+
+eye_character = input("Please input a character to use as the robots eyes: ")
+
+print("Beep's expression is now as follows:\n")
+
+print("##########")
+print("#  "+eye_character+"  "+eye_character+"  #")
+print("#        #")
+print("##########")
+
+
