4158451351e014df68d153c7b5ca8a0e3d370e08
4badem14 <4badem14@solent.ac.uk>
2022-10-06 11:53:01 +0100

    comparison operators demosntrated

---
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,11 @@
+first_number = int(input("Tell me a number: "))
+second_number = int(input("Tell me another number: "))
+
+if first_number > second_number:
+    print("First number is bigger!")
+
+elif first_number < second_number:
+    print("Second number is bigger!")
+
+else:
+    print("The numbers are the same!")
