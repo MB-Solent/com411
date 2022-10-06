97e7966591f170475cc4b61fae792f01502b9819
4badem14 <4badem14@solent.ac.uk>
2022-10-06 11:34:37 +0100

    demonstrate modulo operator

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,9 @@
+print("Please enter a whole number")
+
+number = int(input())
+
+if (number % 2) == 0:
+    print(f"Thank you! {number} is a whole number.")
+
+else:
+    print(f"Sorry, {number} is not a whole number.")
