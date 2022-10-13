e84091f4cb03941bc719c84e7eb2aeb38925403b
mihai <4badem14@solent.ac.uk>
2022-10-13 15:58:36 +0100

    return_values.py file created and program partially completed

---
+++ basics/functions/return_values.py
@@ -0,0 +1,19 @@
+def sum_weights(weight1, weight2):
+    pass
+
+
+def calc_avg_weight(weight1, weight2):
+    pass
+
+
+def get_bot_weight(bot_name):
+    weight = input(f" What is {bot_name}'s weight?: ")
+    return weight
+
+
+def run():
+
+    weight1 = get_bot_weight("Beep")
+    weight2 = get_bot_weight("Bop")
+
+
