afda264a54e057b61a54fbbc5c64a0fc6f9f8b83
mihai <4badem14@solent.ac.uk>
2022-10-13 12:23:04 +0100

    function with nesting program completed

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,12 @@
+def identify():
+    print("What do you see?")
+    object = input()
+
+    if object == "a large boulder":
+        print("It's time to run!")
+
+    else:
+        print("cope")
+
+
+identify()
