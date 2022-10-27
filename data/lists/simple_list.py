0175fad8d34e27861a015a4b3c1f11ef557889a4
Mihai <4badem14@solent.ac.uk>
2022-10-27 14:17:12 +0100

    activity 1 completed

---
+++ data/lists/simple_list.py
@@ -0,0 +1,12 @@
+def directions():
+    directions_list = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return directions_list
+
+
+def run():
+    print(directions())
+    pass
+
+
+if __name__ == "__main__":
+    run()
