a9db1a157949f7c314aea37841b0dd286a654d28
mihai <4badem14@solent.ac.uk>
2022-11-03 11:59:44 +0000

    simple_set.py created, task completed

---
+++ data/sets/simple_set.py
@@ -0,0 +1,12 @@
+def observed():
+    observations = {"Flying Car", "Sky Scraper", "Laser", "Dome"}
+    return observations
+    pass
+
+
+def run():
+    print(observed())
+
+
+if __name__ == "__main__":
+    run()
