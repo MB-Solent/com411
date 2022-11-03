1656f9bc6810b920376c61857467d9403afd9179
mihai <4badem14@solent.ac.uk>
2022-11-03 14:25:13 +0000

    finished task regarding sorted set and removing items from observations list

--- data/sets/sorted_set.py
+++ data/sets/sorted_set.py
@@ -23,15 +23,31 @@ def remove_observations(observations):
             remove = input()
 
         if remove == "Yes":
+            print("Which observation would you like to remove?: ")
+            choice = input()
+
+            if choice in observations:
+                observations.remove(choice)
+                print(f'Removed first instance of "{choice}" from the list of observations.')
+            else:
+                print("Choice inputted was not found in the list of observations.")
+
+            print("Would you like to remove another observation? (Yes) or (No): ")
 
 
 def run():
     print("Counting observations...")
+
     observations = observed()
+
+    remove_observations(observations)
+
     observations_count = set()
 
     for item in observations:
         observations_count.add((item, observations.count(item)))
+
+    print(sorted(observations_count))
 
     for count in observations_count:
         print(f"{count[0]} appeared {count[1]} time(s).")
